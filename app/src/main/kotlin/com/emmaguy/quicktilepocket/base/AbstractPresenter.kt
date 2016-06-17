2c75c167368155612c509a1fda5804a45320d179
Emma Guy <emmag87@gmail.com>
2016-06-17 20:48:21 +0100

    Add logging

--- app/src/main/kotlin/com/emmaguy/quicktilepocket/base/AbstractPresenter.kt
+++ app/src/main/kotlin/com/emmaguy/quicktilepocket/base/AbstractPresenter.kt
@@ -2,12 +2,14 @@ package com.emmaguy.quicktilepocket.base
 
 import rx.Subscription
 import rx.subscriptions.CompositeSubscription
+import timber.log.Timber
 
 abstract class AbstractPresenter<in V : AbstractPresenter.View> {
     private val subscriptions: CompositeSubscription = CompositeSubscription()
     private var view: View? = null
 
     open fun onAttachView(view: V) {
+        Timber.d("onAttachView: " + view)
         if (this.view !== null) {
             throw IllegalStateException("View " + this.view + " has already been attached")
         }
@@ -16,6 +18,7 @@ abstract class AbstractPresenter<in V : AbstractPresenter.View> {
     }
 
     open fun onDetachView() {
+        Timber.d("onDetachView: " + view)
         if (this.view == null) {
             throw IllegalStateException("View has already been detached")
         }
--- app/src/main/kotlin/com/emmaguy/quicktilepocket/feature/PocketQuickSettingsTileService.kt
+++ app/src/main/kotlin/com/emmaguy/quicktilepocket/feature/PocketQuickSettingsTileService.kt
@@ -1,15 +1,19 @@
 package com.emmaguy.quicktilepocket.feature
 
+import android.content.Intent
+import android.os.IBinder
 import android.service.quicksettings.TileService
 import com.emmaguy.quicktilepocket.Inject
 import com.emmaguy.quicktilepocket.Injector
 import com.emmaguy.quicktilepocket.R
+import timber.log.Timber
 import java.util.*
 
 class PocketQuickSettingsTileService : TileService(), Injector by Inject.instance {
     override fun onStartListening() {
         super.onStartListening()
 
+        Timber.d("onStartListening, unread count: ${userStorage.unreadCount}")
         if (userStorage.unreadCount >= 0) {
             qsTile.label = String.format(Locale.getDefault(), getString(R.string.unread_articles_format),
                     userStorage.unreadCount)
@@ -18,5 +22,31 @@ class PocketQuickSettingsTileService : TileService(), Injector by Inject.instanc
         }
 
         qsTile.updateTile()
+    }
+
+    override fun onStopListening() {
+        super.onStopListening()
+
+        Timber.d("onStopListening")
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        Timber.d("onBind")
+        return super.onBind(intent)
+    }
+
+    override fun onTileAdded() {
+        Timber.d("onTileAdded")
+        super.onTileAdded()
+    }
+
+    override fun onTileRemoved() {
+        Timber.d("onTileRemoved")
+        super.onTileRemoved()
+    }
+
+    override fun onClick() {
+        Timber.d("onClick")
+        super.onClick()
     }
 }
